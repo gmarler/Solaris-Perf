7f223fd2e2321ead73429eb9a81bdbc72780fbbd
Gordon Marler <gmarler@gmarler.com>
2013-10-01 19:12:14 -0400

    Eliminate local config for Test::DBIx::Class

--- solaris_perf_local.pl
+++ solaris_perf_local.pl
@@ -1,14 +1,14 @@
-{
-  name => "Solaris::Perf",
-
-  "Model::DB" => {
-    schema_class => "Solaris::Perf::Schema",
-
-    connect_info => {
-      dsn          => "dbi:Pg:dbname=perfdb",
-      user         => "catalyst",
-      password     => "test",
-      AutoCommit   => 1,
-    }
-  }
-}
+#{
+#  name => "Solaris::Perf",
+#
+#  "Model::DB" => {
+#    schema_class => "Solaris::Perf::Schema",
+#
+#    connect_info => {
+#      dsn          => "dbi:Pg:dbname=perfdb",
+#      user         => "catalyst",
+#      password     => "test",
+#      AutoCommit   => 1,
+#    }
+#  }
+#}
