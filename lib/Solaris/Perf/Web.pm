ae6752fb135201b61a82b0add0eaebc025d06fc2
Gordon Marler <gmarler@gmarler.com>
2014-12-23 09:44:04 -0500

    Set default view to HTML

--- lib/Solaris/Perf/Web.pm
+++ lib/Solaris/Perf/Web.pm
@@ -37,6 +37,7 @@ extends 'Catalyst';
 
 __PACKAGE__->config(
   name => 'Solaris::Perf::Web',
+  default_view => 'HTML',
   # Disable deprecated behavior needed by old applications
   disable_component_resolution_regex_fallback => 1,
   enable_catalyst_header => 1, # Send X-Catalyst header
