9e184069e9f995294931f1352d421cc1b27ae25c
Gordon Marler <gmarler@gmarler.com>
2014-12-25 02:22:47 -0500

    Use proper load_components list for Solaris::Perf::Schema::ResultSet

--- lib/Solaris/Perf/Schema/ResultSet.pm
+++ lib/Solaris/Perf/Schema/ResultSet.pm
@@ -5,6 +5,11 @@ use warnings;
  
 use base 'DBIx::Class::ResultSet';
  
-__PACKAGE__->load_components('Helper::ResultSet');
+__PACKAGE__->load_components(
+  'Helper::ResultSet'
+  # With the above, are the ones below even needed?
+  #'Helper::ResultSet::IgnoreWantarray',
+  #'Helper::ResultSet::Me',
+);
  
 1;
