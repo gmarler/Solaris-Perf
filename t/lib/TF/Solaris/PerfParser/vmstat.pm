db1a69dffeaddfe5ae399acb57a6b29c87832676
Gordon Marler <gmarler@gmarler.com>
2014-12-26 12:03:46 -0500

    Add utility functions to generate identical vmstat output for any amount of time

--- t/lib/TF/Solaris/PerfParser/vmstat.pm
+++ t/lib/TF/Solaris/PerfParser/vmstat.pm
@@ -5,9 +5,15 @@ use Test::Class::Moose;
 with 'Test::Class::Moose::Role::AutoUse';
 
 use Data::Dumper;
+use DateTime;
 
 # Set up for schema
 #BEGIN { use Solaris::Perf::Schema; }
+
+# Header for generated/"mocked" vmstat output
+my $periodic_header =
+" kthr      memory            page            disk          faults      cpu
+ r b w   swap  free  re  mf pi po fr de sr s2 s2 s3 s3   in   sy   cs us sy id";
 
 # Concerning this test file:
 # 1. Taken on P315
@@ -75,5 +81,49 @@ sub test_next {
   }
 
   cmp_ok($sample_count, '==', 4, 'Parsed 4 data samples');
+}
+
+sub generate_base_dt {
+  my ($self) = @_;
+
+  my $dt_base = DateTime->new(
+                  year      => 2014,
+                  month     => 10,
+                  day       => 24,
+                  hour      => 0,
+                  minute    => 0,
+                  second    => 0,
+                  time_zone => "local",
+                );
+
+  return $dt_base;
+}
+
+sub generate_vmstat_identical {
+  my ($self,$secs_to_gen) = @_;
+
+  my $dt_base = $self->generate_base_dt();
+  my $dt_iter = $dt_base->clone;
+
+  while ($secs_to_gen--) {
+    say $dt_iter->epoch;
+    if ((($secs_to_gen + 1) % 10) == 0) {
+      say $periodic_header;
+    }
+    my $data = sprintf(" %1lu %1lu %1lu %6u %5u %3.0f %3.0f %2.0f %2.0f" .
+                       " %2.0f %2ld %2.0f %2d %2d %2d %2d %4.0f %4.0f" .
+                       " %4.0f %2.0f %2.0f %2.0f",
+                       3,0,0,1216860,627656,0,115,0,0,0,0,0,0,0,0,0,
+                       8087,106896,10555,2,3,94);
+    say $data;
+    $dt_iter->add( seconds => 1);
+  }
+}
+
+sub generate_vmstat_random {
+  my ($self,$secs_to_gen) = @_;
+
+  my $dt_base = $self->generate_base_dt();
+  my $dt_iter = $dt_base->clone;
 }
 
