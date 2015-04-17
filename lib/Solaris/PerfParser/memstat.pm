2aac67155521260972e4ba6fcd8a5fb1e783053a
Gordon Marler <gmarler@gmarler.com>
2015-04-16 22:16:52 -0400

    parser ready to be tested now

--- lib/Solaris/PerfParser/memstat.pm
+++ lib/Solaris/PerfParser/memstat.pm
@@ -39,38 +39,41 @@ sub _parse_interval {
   my (%memstat_data);
 
   my $memstat_regex =
-    qr{^ (?: \s+? (?<kthr_runq>(?:kthr|r)) [^\n]+ \n |   # vmstat data headers
-             \s+? (?<kthr_runq>\d+) \s+ (?<kthr_blocked>\d+) \s+
-                  (?<kthr_swapped>\d+) \s+
-             (?<swap_avail>\d+) \s+ (?<free_list>\d+) \s+
-             (?<page_reclaims>\d+) \s+ (?<minor_faults>\d+) \s+
-             (?<page_in_KB>\d+) \s+ (?<page_out_KB>\d+) \s+
-             (?<freed_KB>\d+) \s+ (?<shortfall_KB>\d+) \s+
-             (?<scan_rate>\d+) \s+
-             (?<s0>\d+) \s+ (?<s1>\d+) \s+ (?<s2>\d+) \s+ (?<s3>\d+) \s+
-             (?<interrupts>\d+) \s+ (?<syscalls>\d+) \s+
-             (?<context_switches>\d+) \s+ (?<user>\d+) \s+
-             (?<sys>\d+) \s+ (?<idle>\d+)
+    qr{^ (?: \s+? (?<pgtype>(?:Page|---)) [^\n]+ \n |   # memstat data headers
+             \s+? (?<pgtype>(?:Kernel|Defdump\sprealloc|Guest|ZFS\sMetadata|
+                               ZFS\sFile\sData|Anon|Exec\sand\slibs|Page\scache|
+                               Free\s\(cachelist\)|Free\s\(freelist\)|
+                               Total)) \s+
+                  (?<pgcount>\d+) \s+
+                  (?<bytes>\d+) \s+
+                  (?<pct_of_total>\d+)
              \n
          )
       }smx;
 
-  my @keys = qw(kthr_runq  kthr_blocked kthr_swapped swap_avail
-                free_list page_reclaims minor_faults page_in_KB
-                page_out_KB freed_KB shortfall_KB scan_rate
-                s0 s1 s2 s3 interrupts syscalls context_switches
-                user sys idle );
-
-  # We added 'g' here to get them all, one by one
-  # There's only one line per interval though, so not much to do.
-  while ($data =~ m{ $vmstat_regex }gsmx ) {
+  while ($data =~ m{ $memstat_regex }gsmx ) {
     # Skip headers
-    next if ($+{kthr_runq} =~ m{^(?:kthr|r)$} );
+    next if ($+{pgtype} =~ m{^(?:Page|---)$} );
 
-    my %href = map { $_ => $+{$_} } @keys;
+    my $page_type = $+{pgtype};
+    $memstat_data{$page_type} = { };
 
-    # push @{$vmstat_data{'vm_data'}}, \%href;
-    $vmstat_data{'vm_data'} = \%href;
+    my ($page_count,$bytes,$pct_of_total) = $+{'pgcount','bytes','pct_of_total'};
+
+    if ($bytes =~ m/(?<size>\d+)(?<unit>k|M|G|)$/) {
+      $bytes = $+{size};
+      if ($+{unit} eq '') { # must be in bytes, nothing to do
+      } elsif ($+{unit} eq 'k') {
+        $bytes *= 1024;
+      } elsif ($+{unit} eq 'M') {
+        $bytes *= 1024 * 1024;
+      } elsif ($+{unit} eq 'G') {
+        $bytes *= 1024 * 1024 * 1024;
+      }
+    }
+
+    $memstat_data{$page_type}->{'page_count','bytes','pct_of_total'} =
+      ($page_count, $bytes, $pct_of_total);
   }
 
   return \%memstat_data;
